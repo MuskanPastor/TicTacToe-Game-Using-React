324f4629ef3c11e712c99a226442c2ca56cfbc5c
Muskan Pastor <pastormuskan240@gmail.com>
2024-06-26 15:18:09 +0530

    added=

--- src/Components/TicTacToe/TicTacToe.jsx
+++ src/Components/TicTacToe/TicTacToe.jsx
@@ -2,7 +2,7 @@ import React, { useRef, useState } from 'react'
 import './TicTacToe.css'
 import circle_icon from '../Assets/circle.png'
 import cross_icon from '../Assets/cross.png' 
-let data=["","","","","","","","",""]
+let data= ["","","","","","","","",""] 
 const TicTacToe = () => {
 let [count,setCount]=useState(0);
 let [lock,setLock]=useState(false);
@@ -35,28 +35,28 @@ const toggle=(e,num) =>{
 }
 
 const checkWin=() =>{
-    if(data[0]==data[1] && data[1]==data[2] && data[2]!=""){
+    if(data[0]===data[1] && data[1]===data[2] && data[2]!==""){
        won(data[2]);
     }
-    else if(data[3]==data[4] && data[4]==data[5] && data[5]!=""){
+    else if(data[3]===data[4] && data[4]===data[5] && data[5]!==""){
         won(data[5]);
      }
-    else if(data[6]==data[7] && data[7]==data[8] && data[8]!=""){
+    else if(data[6]===data[7] && data[7]===data[8] && data[8]!==""){
         won(data[8]);
      } 
-    else if(data[0]==data[3] && data[3]==data[6] && data[6]!=""){
+    else if(data[0]===data[3] && data[3]===data[6] && data[6]!==""){
         won(data[6]);
      }
-     else if(data[1]==data[4] && data[4]==data[7] && data[7]!=""){
+     else if(data[1]===data[4] && data[4]===data[7] && data[7]!==""){
         won(data[7]);
      }
-     else if(data[2]==data[5] && data[5]==data[8] && data[8]!=""){
+     else if(data[2]===data[5] && data[5]===data[8] && data[8]!==""){
         won(data[8]);
      }
-     else if(data[0]==data[4] && data[4]==data[8] && data[8]!=""){
+     else if(data[0]===data[4] && data[4]===data[8] && data[8]!==""){
         won(data[8]);
      } 
-     else if(data[2]==data[4] && data[4]==data[6] && data[6]!=""){
+     else if(data[2]===data[4] && data[4]===data[6] && data[6]!==""){
         won(data[6]);
      }
 }
